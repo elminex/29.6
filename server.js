e5db2b1f2f5ab72289234c2dcb44c702f905db3c
Paweł Tuliński <pawel.tulinski@gmail.com>
2019-07-19 12:37:04 +0200

    Add database

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://databaseUser:<password>@cluster0-hegcg.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://databaseUser:<devilek123>@cluster0-hegcg.mongodb.net/test?retryWrites=true&w=majority', {
 });
 
 //new user Schema
